87b53f50f6af1055f0379d3003993a575e681d01
Danila <dod.shreder@gmail.com>
2023-08-19 15:45:44 +0300

    i added bootstrap and fixed problem with css includings

--- app/views/main.php
+++ app/views/main.php
@@ -1,2 +1,21 @@
 <?php
 session_start();
+
+?>
+
+<style>
+    <?php include "../../bootstrap/css/bootstrap.min.css" ?>
+    <?php include "css/main.css" ?>
+</style>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Главная</title>
+</head>
+<body>
+fecsd
+</body>
+</html>
--- app/views/sign-up.php
+++ app/views/sign-up.php
@@ -7,85 +7,8 @@ if (isset($_SESSION['user'])) {
 ?>
 
 <style>
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
-
-    body {
-        background: #ff6c00;
-        margin: 0;
-        background-image: url("img/back.png");
-    }
-
-    h2 {
-        margin: 0;
-        text-transform: uppercase;
-        padding-bottom: 5px;
-        border-bottom: 3px solid #5200a5;
-    }
-
-    form {
-        margin: 0 auto;
-        background: rgba(0, 186, 255, 0.77);
-        width: 470px;
-        height: 470px;
-        padding: 20px;
-        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.82);
-    }
-
-    .group {
-        padding: 5px;
-    }
-
-    label {
-        padding-left: 10px;
-        text-transform: uppercase;
-    }
-
-    input {
-        margin-top: 3px;
-        height: 40px;
-        width: 400px;
-        border-radius: 20px/20px;
-        border: none;
-        padding-left: 15px;
-        font-size: 18px;
-        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.82);
-    }
-
-    input:focus {
-        border: 2px solid #6c00ff;
-    }
-
-    button {
-        cursor: pointer;
-        padding: 10px 30px;
-        height: 50px;
-        color: #fff;
-        background: #5200a5;
-        border: none;
-        text-transform: uppercase;
-        font-size: 15px;
-        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.82);
-    }
-
-    a, a:hover {
-        text-decoration: none;
-    }
-
-    button:hover {
-        font-weight: bold;
-    }
-
-    .msg {
-        border: 2px solid #b67212;
-        border-radius: 3px;
-        padding: 10px;
-        text-align: center;
-        font-weight: bold;
-    }
+    <?php include "../../bootstrap/css/bootstrap.min.css" ?>
+    <?php include "css/sign-up.css" ?>
 </style>
 
 <!DOCKTYPE html>
